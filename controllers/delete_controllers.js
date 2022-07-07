c5ebe6a725faf9def60d42a96ec9009a6a476d96
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-07-07 15:23:43 +0100

    Pushing controller and model files.

---
+++ controllers/delete_controllers.js
@@ -0,0 +1,10 @@
+const {removeComment} = require('../models/delete_models');
+
+exports.deleteComment = (req, res, next) => {
+    const { comment_id } = req.params;
+    removeComment(comment_id)
+    .then((deletedComment) => {
+        if (deletedComment.rows.length !== 0) {res.status(204).send();}
+        next({status: 404, message: "Comment not found."})
+    });
+}

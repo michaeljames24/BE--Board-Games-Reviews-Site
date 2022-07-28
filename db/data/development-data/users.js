29358f71e4ab9079497f73e9b3cbaef63692acce
michaeljames24 <93047055+michaeljames24@users.noreply.github.com>
2022-07-28 14:58:41 +0100

    Add default user to db.

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -35,4 +35,10 @@ module.exports = [
     avatar_url:
       'https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141',
   },
+  {
+    username: 'default_user123',
+    name: 'Default User',
+    avatar_url:
+      'https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg',
+  }
 ];
